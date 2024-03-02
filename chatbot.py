e567fc750302d56d6e962fd0b6ad91b48be5d530
Seoyoung Cho <cho411@purdue.edu>
2024-03-02 16:31:26 -0500

    chatbot proto

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+import gradio as gr
+import os
+import openai
+
+openai.api_key = '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'
+message_history = []
+message_to_send = ""
+def chat_response(message, history):
+    user_message = {"role":"user","content": message}
+    message_history.append(user_message)
+    chat_completion = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=message_history)
+    choices = chat_completion['choices']
+    chatgpt_response = choices[0]['message']
+    content = chatgpt_response['content']
+    chatbot_response = {"role":"assistant","content": content}
+    message_history.append(chatbot_response)
+    return content
+
+chat_demo = gr.ChatInterface(chat_response)
+chat_demo.launch()
